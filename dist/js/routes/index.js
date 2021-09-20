c2e0323af0859725820516feabad560177253a51
admin <admin@Shariques-MacBook-Pro.local>
2021-09-20 16:12:20 +0530

    remove gitignore files

--- dist/js/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const books_1 = require("../controllers/books");
-const router = (0, express_1.Router)();
-router.get("/books", books_1.getBooks);
-router.get("/book/:id", books_1.getBook);
-exports.default = router;
